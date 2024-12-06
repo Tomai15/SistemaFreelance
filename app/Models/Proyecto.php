3b4c4a33b8e9b589b074c124542ad2c940ea5f2e
Delgado Tomas <tdelgadoandrade@frba.utn.edu.ar>
2024-12-06 10:31:10 -0300

    Avance en algunas tablas

--- app/Models/Proyecto.php
+++ app/Models/Proyecto.php
@@ -3,9 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Proyecto extends Model
 {
     protected $table = 'proyecto';
     public $timestamps = false;
+
+    public function tecnologias(): HasMany
+    {
+        return $this->hasMany(Tecnologia::class);
+    }
+    public function estadosProyecto(): HasMany
+    {
+        return $this->hasMany(EstadoProyecto::class);
+    }
+    public function tagsBusqueda(): HasMany
+    {
+        return $this->hasMany(TagBusqueda::class);
+    }
+
+    
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,19 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+
+    public function proyectos(): HasMany
+    {
+        return $this->hasMany(Proyecto::class);
+    }
+
+
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
