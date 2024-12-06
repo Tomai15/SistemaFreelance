7d7278e4cfb01c4d5028be12a5f49e1420770e80
Delgado Tomas <tdelgadoandrade@frba.utn.edu.ar>
2024-12-06 12:23:32 -0300

    Creo que ya estan terminadas todas las tablas

--- app/Models/Proyecto.php
+++ app/Models/Proyecto.php
@@ -3,25 +3,46 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Proyecto extends Model
 {
     protected $table = 'proyecto';
     public $timestamps = false;
 
-    public function tecnologias(): HasMany
+    public function tecnologias(): BelongsToMany
     {
-        return $this->hasMany(Tecnologia::class);
+        return $this->belongsToMany(Tecnologia::class,'tecnologia_por_proyecto');
     }
+
     public function estadosProyecto(): HasMany
     {
         return $this->hasMany(EstadoProyecto::class);
     }
-    public function tagsBusqueda(): HasMany
+
+    public function estadoActual(): HasOne
     {
-        return $this->hasMany(TagBusqueda::class);
+        return $this->estadosProyecto()->one()->latestOfMany();
     }
+
+    public function tagsBusqueda(): BelongsToMany
+    {
+        return $this->belongsToMany(TagBusqueda::class,'tag_por_proyecto');
+    }
+
+    public function postulantes(): BelongsToMany
+    {
+        return $this->belongsToMany(PerfilDesarrollador::class,'postulante_por_proyecto');
+    }
+
+    public function desarrolladorSeleccionado(): HasOne
+    {
+        return $this->hasOne(PerfilDesarrollador::class);
+    }
+
+
 
     
 }
