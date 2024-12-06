cabe4b1458cc5e61a11594cfba6be7bd70cc69ac
Delgado Tomas <tdelgadoandrade@frba.utn.edu.ar>
2024-12-06 18:17:11 -0300

    Todas las relaciones completas

--- app/Models/EstadoProyecto.php
+++ app/Models/EstadoProyecto.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class EstadoProyecto extends Model
 {
-    //
+    public function tipoEstado(): HasOne
+    {
+        return $this->hasOne(TipoEstadoProyecto::class);
+    }
 }
