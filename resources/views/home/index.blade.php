6c7b4f457d453f5eb050b89a8f1eae8e4f8fe4ff
matias escordamaglia <mescordamaglia@frba.utn.edu.ar>
2024-12-08 22:17:58 -0300

    Vistas planteadas

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,7 @@
+@include('layout.header')
+
+<div>
+    <h1>Este sería el home</h1>
+</div>
+
+@include('layout.footer')
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header con Bootstrap</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href=".../css/header.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand logo" href="#">Logo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Servicios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contacto</a>
+                        </li>
+                    </ul>
+
+                    <div class="d-flex">
+                        <button class="btn btn-outline-primary me-2" type="button">Iniciar Sesión</button>
+                        <button class="btn btn-primary btn-register" type="button">Registrarse</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
