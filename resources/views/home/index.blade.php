90ede0ef1a0e10c41a3e24299151198eaab8a524
Tomas Delgado <tdelgadoandrade@frba.utn.edu.ar>
2024-12-13 14:12:06 -0300

    Mejora en la pagina de home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,16 @@
 @include('layout.header')
 
-<div>
-    <h1>Este sería el home</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+            <p>
+                Freelance por plata es un servicio de freenlance que conecta a personas con
+                nececidad de algun desarrollo de software y personas con la habilidad para desarrollarlo
+            </p>
+        </div>
+
+    </div>
 </div>
+
 
 @include('layout.footer')
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header con Bootstrap</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href=".../css/header.css">
+    <link rel="stylesheet" href="{{asset('css/header.css')}}">
 </head>
 <body>
     <header>
         <nav class="navbar navbar-expand-lg bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand logo" href="#">Logo</a>
+                <img src="{{asset('imagensPresentacion/Logo.jpg')}}" class="navbar-brand logo">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
