641e3ad18d1942ca79bd5e4b39f843bc87eefdfd
Carolina <crinaldi@frba.utn.edu.ar>
2024-12-14 13:57:56 -0300

    se agrega el footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,2 +1,63 @@
-    </body>
+<footer class="bg-dark text-white pt-5 pb-4">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-4 mb-4">
+                <h5 class="text-uppercase mb-3">Redes Sociales</h5>
+                <a href="#" class="text-white me-3">
+                    <i class="bi bi-facebook"></i> Facebook
+                </a><br>
+                <a href="#" class="text-white me-3">
+                    <i class="bi bi-twitter"></i> Twitter
+                </a><br>
+                <a href="#" class="text-white me-3">
+                    <i class="bi bi-instagram"></i> Instagram
+                </a><br>
+                <a href="#" class="text-white me-3">
+                    <i class="bi bi-linkedin"></i> Linkedin
+                </a><br>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <h5 class="text-uppercase mb-3">Contáctanos</h5>
+                <form>
+                    <div class="mb-3 me-3">
+                        <input type="text" class="form-control" placeholder="Nombre">
+                    </div>
+                    <div class="mb-3 me-3">
+                        <input type="email" class="form-control" placeholder="Correo Electrónico">
+                    </div>
+                    <div class="mb-3 me-3">
+                        <textarea class="form-control" rows="3" placeholder="Mensaje"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <h5 class="text-uppercase mb-3">Sobre Nosotros</h5>
+                <p> Somos un equipo de desarrolladores comprometidos para ofrecer los mejores servicios, con soluciones innovadoras y personalizadas, a nuestros clientes</p>
+                <div class="row mt-5">
+                    <h6 class="text-uppercase mb-3">Nuestro Equipo</h6>
+                    <div class="col-3">Tomas Delgado</div>
+                    <div class="col-3">Carolina Rinaldi</div>
+                    <div class="col-3">Matias Escordamaglia</div>
+                    <div class="col-3">Mateo Fernandez</div>
+                </div>
+            </div>
+
+        </div>
+        
+        <div class="row mt-4">
+            <div class="col text-center">
+                <p>&copy; 2024 Freenlance. Todos los derechos reservados.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
 </html>
