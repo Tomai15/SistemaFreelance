0a0ba6d7d58e29e190163dd566a06f255bd27010
Carolina <crinaldi@frba.utn.edu.ar>
2024-12-30 13:36:00 -0300

    mejora visual en home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,32 +2,33 @@
 
 <main class="">
 
-    <div class="d-flex align-items-center image text-gray">
+    <div class="d-flex align-items-center background text-gray">
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <h1 class="display-4">Freelance por plata</h1>
-                    <p class="lead">Es un servicio de freenlance que conecta a personas con
+                    <h1 class="display-4 fw-bolder">Freelance por plata</h1>
+                    <p class="lead fs-2 my-3">Es un servicio de freenlance que conecta a personas con
                         nececidad de algun desarrollo de software y personas con la habilidad para desarrollarlo.</p>
-                    <a href="#footer" class="btn btn-primary btn-lg">Saber más</a>
+                    <a href="#footer" class=" my-2 btn btn-info btn-lg">Saber más</a>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="d-flex align-items-center">
+    <div class="d-flex align-items-center image">
         <div class="container">
             <div class="row  my-5">
-                <div class="col-md-4">
-                    <h2>Imagen más botón para ver el listado de proyectos</h2>
-                    <a href="/proyectos" class="btn btn-secondary btn-sm my-2">Ver Proyectos</a>
+                <div class="col-md-6 my-2">
+                    
+                    <a href="/proyectos" class="btn btn-light border btn-lg fs-4 display-6 p-3 ms-5">Accerder a proyectos disponiles</a>
                 </div>
-                <div class="col-md-8">
-                    <h3>Texto explicativo de la funcion</h3>
+                <div class="col-md-6 my-2">
+                    <a href="/proyectos/create" class="btn btn-light border fs-4 btn-lg p-3 ms-5">Publica tu proyecto a concretar</a>
+                   {{--  <h3>Texto explicativo de la funcion</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel velit neque. Fusce id lectus
                         id neque vehicula luctus.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin auctor justo vitae justo
-                        ultricies, ac luctus metus ultricies.</p>
+                        ultricies, ac luctus metus ultricies.</p> --}}
                 </div>
             </div>
         </div>
